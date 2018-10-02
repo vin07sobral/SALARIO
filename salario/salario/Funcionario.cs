ecf0fcbda93201178520b3ef26385c8c0a73b3e9
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-10-02 13:37:11 -0300

    Add files via upload

---
+++ salario/salario/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+namespace introducao_poo
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salarioBruto;
+        public double desconto;
+        public double salarioLiquido()
+        {
+            return salarioBruto - desconto;
+        }
+        public void aumentarSalario(double porcentagem)
+        {
+            salarioBruto = salarioBruto + (salarioBruto * porcentagem / 100.0);
+        }
+        public override string ToString()
+        {
+            return nome
+            + ", R$ "
+            + salarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ salario/salario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace introducao_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario x = new Funcionario();
+            double porcent;
+            Console.Write("Nome: ");
+            x.nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            x.salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Desconto: ");
+            x.desconto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Dados do funcionário: " + x);
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.aumentarSalario(porcent);
+            Console.WriteLine();
+            Console.WriteLine("Dados do funcionário: " + x);
+            Console.ReadLine();
+        }
+    }
+}
